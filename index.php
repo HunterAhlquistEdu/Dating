ab72529464d8794e0a571cd9ecf36a43e8bcd7b3
HunterAhlquistEdu <hunterxahlquist@gmail.com>
2021-05-13 10:55:24 -0700

    signup first page

--- index.php
+++ index.php
@@ -1,10 +1,40 @@
 <?php
-//initialize F3
+//Turn on error-reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Start a session
+session_start();
+
+
+//Require necessary files
 require('vendor/autoload.php');
-$f3 = \Base::instance();
+
+//initialize F3
+$f3 = Base::instance();
+
 $f3->route('GET /',
     function() {
-        include "views/home.html";
+        //Display the home page
+        $view = new Template();
+        echo $view->render("views/home.html");
+    }
+);
+$f3->route('GET /create-account',
+    function() {
+        //Display the home page
+        $view = new Template();
+        echo $view->render("views/csignup.html");
+    }
+);
+$f3->route('GET /create-shipping',
+    function() {
+        include "views/cshipping.html";
+    }
+);
+$f3->route('GET /create-interests',
+    function() {
+        include "views/cinterests.html";
     }
 );
 $f3->run();
