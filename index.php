849188af4b86debae7c22f6a015284714e110b31
HunterAhlquistEdu <hunterxahlquist@gmail.com>
2021-05-13 17:18:56 -0700

    shipping and interests selection

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ session_start();
 
 
 //Require necessary files
-require('vendor/autoload.php');
+require_once ('vendor/autoload.php');
+require_once ('model/data_layer.php');
 
 //initialize F3
 $f3 = Base::instance();
@@ -20,21 +21,56 @@ $f3->route('GET /',
         echo $view->render("views/home.html");
     }
 );
-$f3->route('GET /create-account',
+$f3->route('GET /home',
+    function($f3) {
+        $f3->reroute('/');
+    }
+);
+$f3->route('GET|POST /create-account',
     function() {
-        //Display the home page
+        //clear session
+        $_SESSION = array();
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_SESSION['firstname'] = $_POST['firstname'];
+            $_SESSION['lastname'] = $_POST['lastname'];
+            $_SESSION['budget'] = $_POST['budget'];
+            $_SESSION['cpu-brand'] = $_POST['cpu-brand'];
+            $_SESSION['email'] = $_POST['email'];
+
+            header('location: create-shipping');
+        }
+
+        //Render page
         $view = new Template();
         echo $view->render("views/csignup.html");
     }
 );
-$f3->route('GET /create-shipping',
+$f3->route('GET|POST /create-shipping',
     function() {
-        include "views/cshipping.html";
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_SESSION['phone'] = $_POST['phone'];
+            $_SESSION['state'] = $_POST['state'];
+            $_SESSION['address'] = $_POST['address'];
+            $_SESSION['postal'] = $_POST['postal'];
+
+            header('location: create-interests');
+        }
+
+        //Render page
+        $view = new Template();
+        echo $view->render("views/cshipping.html");
     }
 );
-$f3->route('GET /create-interests',
-    function() {
-        include "views/cinterests.html";
+$f3->route('GET|POST /create-interests',
+    function($f3) {
+        $f3->set('interests', getInterests());
+
+
+        //Render page
+        $view = new Template();
+        echo $view->render("views/cinterests.html");
     }
 );
+
 $f3->run();
---
+++ model/data_layer.php
@@ -0,0 +1,5 @@
+<?php
+function getInterests() {
+    return array("gaming", "video editing", "autoCAD", "3D rendering", "development", "live streaming", "writing",
+        "watching video", "web hosting", "Crypto Mining", "NAS hosting");
+}
